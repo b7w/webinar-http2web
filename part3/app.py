f35f91dd83eb1bc6a3f5f9e2d78008467e8a1351
b7w@isudo.ru <b7w@isudo.ru>
2020-06-12 18:57:08 +0300

    Add part 3

---
+++ part3/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask('http2web')
+
+app.counter = 0
+
+
+@app.route('/')
+def index():
+    with open('index.html') as f:
+        html = f.read()
+        return html.replace('VARIABLE', str(app.counter))
+
+
+@app.route('/increment')
+def increment():
+    app.counter += 1
+    return str(app.counter)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
